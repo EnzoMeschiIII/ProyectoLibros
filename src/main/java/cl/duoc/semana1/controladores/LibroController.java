b89e5088941732d8c14b5e6408382dab424672e3
EnzoMeschiIII <e.meschi@duocuc.cl>
2025-10-25 18:45:29 -0300

    Hice cambios agregue 2 metodos que faltaban

--- src/main/java/cl/duoc/semana1/controladores/LibroController.java
+++ src/main/java/cl/duoc/semana1/controladores/LibroController.java
@@ -4,8 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,45 @@ public class LibroController {
         return libroService.obtenerLibroPorId(id);
     }
 
-    
+    @PostMapping
+    public ResponseEntity<Libro> guardarLibro(@RequestBody Libro libro)
+    {
+        Libro nuevoLibro = libroService.guardarLibro(libro);
+        return ResponseEntity.ok(nuevoLibro);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Libro> actualizarLibro(@PathVariable int id, @RequestBody Libro libro)
+    {
+        Optional<Libro> libroExistente = libroService.obtenerLibroPorId(id);
+        if(libroExistente.isPresent())
+        {
+            Libro lib = libroExistente.get();
+            lib.setTitulo(libro.getTitulo());
+            lib.setAutor(libro.getAutor());
+            lib.setFecha(libro.getFecha());
+            lib.setGenero(libro.getGenero());
+            Libro libroActualizado = libroService.guardarLibro(lib);
+            return ResponseEntity.ok(libroActualizado);
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarLibro(@PathVariable int id)
+    {
+        Optional<Libro> libroExistente = libroService.obtenerLibroPorId(id);
+        if(libroExistente.isPresent())
+        {
+            libroService.eliminarLibro(id);
+            return ResponseEntity.ok().build();
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
